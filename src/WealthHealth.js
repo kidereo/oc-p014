1900e82823550fa85260fa98446d6f1e595c2853
kidereo <kirill.derevenski@gmail.com>
2023-01-20 15:33:15 +0100

    Suspense with lazy loading on Routes and key components.

--- src/WealthHealth.js
+++ src/WealthHealth.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import Header from './components/Header';
-import EmployeeList from './components/EmployeeList';
-import AddEmployee from './components/AddEmployee';
-import Footer from './components/Footer';
+import React, {lazy, Suspense} from 'react';
 import {AppProvider, AppContext} from "./context/AppState";
 import {ToastContainer} from 'react-toastify';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const EmployeeList = lazy(() => import('./components/EmployeeList'));
+const AddEmployee = lazy(() => import('./components/AddEmployee'));
+const renderLoader = () => '';
 
 /**
  * Top app tree.
@@ -25,10 +27,12 @@ function WealthHealth() {
                                     <Header/>
                                 </header>
                                 <main>
-                                    <Routes>
-                                        <Route index element={<EmployeeList/>}/>
-                                        <Route path="/add-employee" element={<AddEmployee/>}/>
-                                    </Routes>
+                                    <Suspense fallback={renderLoader()}>
+                                        <Routes>
+                                            <Route index element={<EmployeeList/>}/>
+                                            <Route path="/add-employee" element={<AddEmployee/>}/>
+                                        </Routes>
+                                    </Suspense>
                                 </main>
                                 <footer>
                                     <Footer/>
